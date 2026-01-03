88a01ab176628a032acaa3f76b4998c6c412cf11
emmy grammy <ojeabuo24@gmail.com>
2026-01-04 00:18:08 +0100

    App.js index.css

--- src/App.js
+++ src/App.js
@@ -10,19 +10,20 @@ const messages = [
 
 
 function App() {
+  const step = 2;
   return (
     <div className="steps">
       <div className="numbers">
-      <div> 1</div>
-       <div> 2</div>
-        <div> 3</div>
+      <div className={step === 1 ? 'active' : ''}> 1</div>
+       <div className={step === 2 ? 'active' : ''}> 2</div>
+        <div className={step === 3 ? 'active' : ''}> 3</div>
         </div>
 
-        <div className='message'> step1: learn react app</div>
+        <div className='message'> step {step}: {messages[step-1]}</div>
        
         <div className='buttons'>
-        <button> previous </button>
-        <button> next </button>
+        <button style={{backgroundColor:'#7950f2', color:'#fff'}}> previous </button>
+        <button style={{backgroundColor:'#7950f2', color:'#fff'}}> next </button>
         </div>
 
     </div>
