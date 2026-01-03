095cddbf59055f8b24a00cc21868583055885c6a
emmy grammy <ojeabuo24@gmail.com>
2026-01-03 23:31:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+      <div> 1</div>
+       <div> 2</div>
+        <div> 3</div>
+        </div>
+
+        <div className='message'> step1: learn react app</div>
+       
+        <div className='buttons'>
+        <button> previous </button>
+        <button> next </button>
+        </div>
+
     </div>
   );
 }
